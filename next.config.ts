dbcaa795753ee2e6a5d665f5327c6ffc483de00c
kenny555max <69280199+kenny555max@users.noreply.github.com>
2025-04-09 23:28:24 +0100

    Update and rename next.config.js to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
- experimental: {
-    turbo: false
+  turbopack: {
+    enabled: true
   }
 };
 
